f72e3903f3a9bb7e378bb19cb6a346869b256e34
Canek <canekcambrayherrera@gmail.com>
2025-06-23 21:05:56 -0700

    Adding fuction declarations for LLM agent to use on a function call plan

--- main.py
+++ main.py
@@ -8,15 +8,51 @@ if len(sys.argv) < 2:
   print("Error: Missing prompt argument pos 1")
   exit(1)
 
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
 prompt = sys.argv[1]
 messages = [ types.Content(role="user", parts=[types.Part(text=prompt)]), ]
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
-content_response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+content_response = client.models.generate_content(
+  model="gemini-2.0-flash-001",
+  contents=messages,
+  config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
 
-print(content_response.text)
+if content_response.function_calls:
+  for f_call in content_response.function_calls:
+    print(f"Calling function: {f_call.name}({f_call.args})")
+else:
+  print(content_response.text)
 
 if "--verbose" in sys.argv:
   print(f"User prompt: {prompt}")
