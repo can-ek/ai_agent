cfcec9bd8b69efedeb62e0247f4f8e5f0d9a7827
Canek <canek@pop-os.local>
2025-07-02 08:55:05 -0700

    Adding function declarations for agent for the remaining functions

--- main.py
+++ main.py
@@ -9,23 +9,72 @@ if len(sys.argv) < 2:
   exit(1)
 
 schema_get_files_info = types.FunctionDeclaration(
-    name="get_files_info",
-    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
-    parameters=types.Schema(
-        type=types.Type.OBJECT,
-        properties={
-            "directory": types.Schema(
-                type=types.Type.STRING,
-                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
-            ),
-        },
-    ),
+  name="get_files_info",
+  description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+  parameters=types.Schema(
+    type=types.Type.OBJECT,
+    properties={
+      "directory": types.Schema(
+        type=types.Type.STRING,
+        description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+      ),
+    },
+  ),
+)
+
+schema_get_file_content = types.FunctionDeclaration(
+  name="get_file_content",
+  description="Gets the contents of a specific file, constrained to the working directory.",
+  parameters=types.Schema(
+    type=types.Type.OBJECT,
+    properties={
+      "file_path": types.Schema(
+        type=types.Type.STRING,
+        description="The path to the file which contents we want to read, relative to the working directory.",
+      ),
+    },
+  ),
+)
+
+schema_run_python = types.FunctionDeclaration(
+  name="run_python",
+  description="Executes a specific python file, constrained to the working directory",
+  parameters=types.Schema(
+    type=types.Type.OBJECT,
+    properties={
+      "file_path": types.Schema(
+        type=types.Type.STRING,
+        description="The path to the python file which we want to run, relative to the working directory.",
+      ),
+    }    
+  )
+)
+
+schema_write_file = types.FunctionDeclaration(
+  name="write_file",
+  description="Modifies the contents of a specific file, constrained to the working directory",
+  parameters=types.Schema(
+    type=types.Type.OBJECT,
+    properties={
+      "file_path": types.Schema(
+        type=types.Type.STRING,
+        description="The path to the file which we want to write contents to, relative to the working directory.",
+      ),
+      "content": types.Schema(
+        type=types.Type.STRING,
+        description="The contents to write to the file"
+      )
+    }    
+  )
 )
 
 available_functions = types.Tool(
-    function_declarations=[
-        schema_get_files_info,
-    ]
+  function_declarations=[
+    schema_get_files_info,
+    schema_get_file_content,
+    schema_run_python,
+    schema_write_file
+  ]
 )
 
 prompt = sys.argv[1]
@@ -36,6 +85,9 @@ You are a helpful AI coding agent.
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
 - List files and directories
+- Read file contents
+- Run python files
+- Write files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
